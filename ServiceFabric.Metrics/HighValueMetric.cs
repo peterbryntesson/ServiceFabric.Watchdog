1bc045419bc79d46f7048a01220214baca85cb8c
Kristofer Liljeblad <kristol@microsoft.com>
2017-12-07 13:39:47 +0100

    Cleaned up some unused usings while looking through the code

--- ServiceFabric.Metrics/HighValueMetric.cs
+++ ServiceFabric.Metrics/HighValueMetric.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ServiceFabric.Metrics
+﻿namespace ServiceFabric.Metrics
 {
     public class HighValueMetric : MetricBase
     {
